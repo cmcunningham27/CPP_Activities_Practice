00bace86b3852e97d3d610ba26d8ad8da8e1be00
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-10-11 12:21:06 -0700

    output two lines, one with a meesage for root1 and its value, the other for root2 and its value

--- Quadratic/quadratic.cpp
+++ Quadratic/quadratic.cpp
@@ -21,5 +21,8 @@ int main() {
     // finds the second root (value of x) using '-'
     root2 = (-b - std::sqrt(b*b - 4*a*c)) / (2*a);
 
+    std::cout << "Root 1 is " << root1 << "\n";
+    std::cout << "Root 2 is " << root2 << "\n";
+
     return 0;
 }
