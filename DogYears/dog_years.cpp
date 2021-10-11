7fbdf28357dda89ea226d033001ccbe714f51a4f
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-10-11 11:46:01 -0700

    declare a function named main

---
+++ DogYears/dog_years.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+// int main() {
+//     int dogyears = 21;
+
+//     int age;
+
+//     std::cout << "Type your dog's age: ";
+//     std::cin >> age;
+
+//     age = age - 2;
+
+//     dogyears = dogyears + (age * 4);
+
+//     std::cout << dogyears;
+
+//     return 0;
+// }
+
+//OR
+
+int main() {}
