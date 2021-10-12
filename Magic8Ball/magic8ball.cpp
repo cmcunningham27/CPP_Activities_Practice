62905a3a491c4f4c91b1c4fb5bac0b5a95a45e64
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-10-12 15:41:46 -0700

    entered output messages for each remainder outcome we can get

--- Magic8Ball/magic8ball.cpp
+++ Magic8Ball/magic8ball.cpp
@@ -11,24 +11,24 @@ int main() {
     int answer = std::rand() % 10;
 
     if (answer == 0) {
-
+        std::cout << "It is certain\n";
     } else if (answer == 1) {
-
+std::cout << "Sign point to yes\n";
     } else if (answer == 2) {
-
+        std::cout << "Ask again later\n";
     } else if (answer == 3) {
-
+        std::cout << "Don't count on it\n";
     } else if (answer == 4) {
-
+        std::cout << "My reply is no\n";
     } else if (answer == 5) {
-
+        std::cout << "Most likely\n";
     } else if (answer == 6) {
-
+        std::cout << "As I see it, yes\n";
     } else if (answer == 7) {
-
+        std::cout << "Better not tell you now\n";
     } else if (answer == 8) {
-
+        std::cout << "Concentrate and ask again\n";
     } else {
-
+        std::cout << "Very doubtful\n";
     };
 }
