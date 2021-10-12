cdcb6e274a905b5927bbfcae0de86c6c8722367a
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-10-12 15:39:01 -0700

    set up random number generator to get 0-9 as remainder, created if/else if statement to check which number 0-9 we got as the remainder

--- Magic8Ball/magic8ball.cpp
+++ Magic8Ball/magic8ball.cpp
@@ -3,5 +3,32 @@
 
 int main() {
     std::cout << "MAGIC 8-BALL: \n\n";
-    
+
+    // sets the seed of the random number generator
+    srand(time(NULL));
+
+    // by dividing the number by 10 and getting the remainder, we get values of 0-9 back
+    int answer = std::rand() % 10;
+
+    if (answer == 0) {
+
+    } else if (answer == 1) {
+
+    } else if (answer == 2) {
+
+    } else if (answer == 3) {
+
+    } else if (answer == 4) {
+
+    } else if (answer == 5) {
+
+    } else if (answer == 6) {
+
+    } else if (answer == 7) {
+
+    } else if (answer == 8) {
+
+    } else {
+
+    };
 }
