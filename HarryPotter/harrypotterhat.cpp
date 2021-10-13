7953ffe16dec473eacb0454dd3fd998b8d7b89fb
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-10-13 12:33:24 -0700

    output the first question

--- HarryPotter/harrypotterhat.cpp
+++ HarryPotter/harrypotterhat.cpp
@@ -6,6 +6,9 @@ int main() {
 
     int answer1, answer2, answer3, answer4;
 
-    std::cout << "The Sorting Hat Quiz!\n";
+    std::cout << "The Sorting Hat Quiz!\n\n";
+
+    std::cout << "Q1) When I'm dead, I want people to remember me as:\n\n";
+    
 
 };
