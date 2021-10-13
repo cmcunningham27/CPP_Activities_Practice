4ed67662f14f04e01424894e18691aca5367660f
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-10-13 13:25:48 -0700

    added comment of the way the game determines who wins

--- RPSLS/rockpapersls.cpp
+++ RPSLS/rockpapersls.cpp
@@ -1,8 +1,20 @@
+/* Rock, Paper, Scissors, Lizard, Spock Game
+Scissors cuts Paper,
+Paper covers Rock,
+Rock crushes Lizard,
+Lizard poisons Spock,
+Spock smashes Scissors,
+Scissors decapitate Lizard,
+Lizard eats Paper,
+Paper disproves Spock,
+Spock vaporizes Rock,
+(and as it always has) Rock crushes Scissors */
+
 #include <iostream>
 #include <stdlib.h>
 
 int main() {
 
 
-    
+
 }
