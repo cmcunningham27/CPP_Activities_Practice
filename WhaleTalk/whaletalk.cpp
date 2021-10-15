91ce187e4d82f09babea46015f6333a8651a5f91
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-10-15 11:27:25 -0700

    declared vector named results to store matching characters

--- WhaleTalk/whaletalk.cpp
+++ WhaleTalk/whaletalk.cpp
@@ -7,7 +7,7 @@ int main() {
     // whales speak with vowels only. E's and u's are longer so need to be doubled
     std::string input = "turpentine and turtles";
     std::vector<char> vowels = {'a', 'e', 'i', 'o', 'u'};
-
-
+    std::vector<char> results;
+    
     return 0;
 };
