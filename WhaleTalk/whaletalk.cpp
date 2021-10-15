48df2c8bf58678299752b5299d73359bf20de6e9
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-10-15 11:26:41 -0700

    declared and initialized vector of vowels to check if string character is a vowel

--- WhaleTalk/whaletalk.cpp
+++ WhaleTalk/whaletalk.cpp
@@ -6,6 +6,8 @@ int main() {
     // initialize input with string we want to convert into whale talk
     // whales speak with vowels only. E's and u's are longer so need to be doubled
     std::string input = "turpentine and turtles";
+    std::vector<char> vowels = {'a', 'e', 'i', 'o', 'u'};
+
 
     return 0;
 };
