a4ae9fc97bffd2a88283be0df181a77a3230490a
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-10-15 11:25:09 -0700

    comment goal and what does whale talk mean, declared and initialized input string to be converted into whale talk

--- WhaleTalk/whaletalk.cpp
+++ WhaleTalk/whaletalk.cpp
@@ -3,7 +3,9 @@
 #include <vector>
 
 int main() {
-
+    // initialize input with string we want to convert into whale talk
+    // whales speak with vowels only. E's and u's are longer so need to be doubled
+    std::string input = "turpentine and turtles";
 
     return 0;
 };
