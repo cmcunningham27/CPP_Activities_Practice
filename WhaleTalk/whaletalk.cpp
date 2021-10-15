58a72c71ba02272e2238e29a67d4a331c25dd914
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-10-15 11:28:45 -0700

    for loop to loop through input string

--- WhaleTalk/whaletalk.cpp
+++ WhaleTalk/whaletalk.cpp
@@ -6,8 +6,15 @@ int main() {
     // initialize input with string we want to convert into whale talk
     // whales speak with vowels only. E's and u's are longer so need to be doubled
     std::string input = "turpentine and turtles";
+    // vector of vowels to check if string characters are vowels or not
     std::vector<char> vowels = {'a', 'e', 'i', 'o', 'u'};
+    // where we will store the vowels from the string
     std::vector<char> results;
-    
+
+    // loop through input string
+    for (int i = 0; i < input.length(); i++) {
+
+    }
+
     return 0;
 };
