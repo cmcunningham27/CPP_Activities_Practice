f223a1a3a438850b33b65db9e8ab545ec465037f
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-10-18 10:46:46 -0700

    output the returned boolean from is_palindrome function when 3 different strings are sent as an argument

--- Palindrome/palindrome.cpp
+++ Palindrome/palindrome.cpp
@@ -9,4 +9,8 @@ bool is_palindrome(std::string text) {
 
 int main() {
 
+    std::cout << is_palindrome("madam") << "\n";
+    std::cout << is_palindrome("ada") << "\n"; 
+    std::cout << is_palindrome("lovelace") << "\n";
+    
 };
