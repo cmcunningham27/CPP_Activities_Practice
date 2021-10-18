d3e81bcc83fe89bd3caa86e0c518c2463950da37
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-10-18 10:48:52 -0700

    interate through string backwards and assign the character to backward each time

--- Palindrome/palindrome.cpp
+++ Palindrome/palindrome.cpp
@@ -3,6 +3,12 @@
 // Check if string is palindrome (same forwards as backwards); return boolean
 bool is_palindrome(std::string text) {
     bool palindrome = false;
+    char backward, forward;
+
+    // iterate through string backwards
+    for (int i = (text.length() - 1); i > -1; i--) {
+        backward = text[i];
+    };
 
     return palindrome;
 };
@@ -12,5 +18,5 @@ int main() {
     std::cout << is_palindrome("madam") << "\n";
     std::cout << is_palindrome("ada") << "\n"; 
     std::cout << is_palindrome("lovelace") << "\n";
-    
+
 };
