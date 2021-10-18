77bd509f89a39133f2d00240039a33be32f445eb
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-10-18 10:44:39 -0700

    include library, begin function to check if string is a palindrome with a string as a parameter, start with a bool variable set to false and then return that boolean after all the checks have completed

---
+++ Palindrome/palindrome.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+// Check if string is palindrome (same forwards as backwards); return boolean
+bool is_palindrome(std::string text) {
+    bool palindrome = false;
+
+    return palindrome;
+};
+
+int main() {
+
+};
