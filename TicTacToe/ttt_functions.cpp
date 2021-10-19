ee498f1e979b0b9633ac8a1b86bdd5f272fcb78d
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-10-19 13:28:39 -0700

    defined display_game function in functions cpp file

---
+++ TicTacToe/ttt_functions.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+void display_game(std::vector<std::string> board) {
+
+    // outputs name of game
+    std::cout << "\nTic-Tac-Toe!\n\n";
+
+    // displays the board with all 9 positions
+    std::cout << " " << board[0] << " | " << board[1] << " | " << board[2] << "\n";
+    std::cout << "___|___|___\n";
+    std::cout << " " << board[3] << " | " << board[4] << " | " << board[5] << "\n";
+    std::cout << "___|___|___\n";
+    std::cout << " " << board[6] << " | " << board[7] << " | " << board[8] << "\n";
+
+};
