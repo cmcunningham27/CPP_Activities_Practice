dfb59180e5ae9eb885da564dea618a00b5596189
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-10-19 13:31:24 -0700

    let players know whose turn it is and what to do next

--- TicTacToe/ttt.cpp
+++ TicTacToe/ttt.cpp
@@ -16,11 +16,14 @@ int main() {
         // determine whose turn it is, starts with Player 1 first (x's)
         if (turns % 2 == 1) {
 
+            std::cout << "Player 1's turn!\n";
+            std::cout << "Pick a position from 1-9 that's not already taken.\n";
 
 
         } else {
 
-
+            std::cout << "Player 2's turn!\n";
+            std::cout << "Pick a position from 1-9 that's not already taken.\n";
 
         };
 
