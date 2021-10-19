419baa612c58a69b81b13ecf17b709ae31d8778f
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-10-19 13:22:37 -0700

    declared and assign vector with 9 strings with spaces

--- TicTacToe/ttt.cpp
+++ TicTacToe/ttt.cpp
@@ -4,6 +4,8 @@
 
 int main() {
 
+    // assign a vector with 9 positions starting with empty strings
+    std::vector<std::string> board = {" ", " ", " ", " ", " ", " ", " ", " ", " "};
 
     return 0;
 };
